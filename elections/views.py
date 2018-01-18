aaf931511ddf79bd0c9fb6f1b11fe146663b1dcc
DS <cckn1985@gmail.com>
2018-01-18 16:39:07 +0900

    Initial commit

---
+++ elections/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Candidate
+# Create your views here.
+
+def index(request):
+    candidates = Candidate.objects.all()
+    str = ''
+
+    for candidate in candidates:
+        str += "<p>{} 기호 {}번({})<br>".format(candidate.name,
+                                      candidate.party_number,
+                                      candidate.area)
+        str+= candidate.inrtoduction
+
+    return HttpResponse(str)
