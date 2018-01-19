67e2d64066c5d1afa578dd2f2aa75426007cfb71
DS <cckn1985@gmail.com>
2018-01-19 14:31:22 +0900

    장고 교육 중

--- cpweb/settings.py
+++ cpweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*nint$c&mxb+k^7yrt86*^yx*)!#-^#cx53$e^61r^$e@2%_wp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
